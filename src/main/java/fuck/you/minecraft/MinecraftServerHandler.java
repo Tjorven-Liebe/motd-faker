43b51d000769390016511ecfa29f4b4fb9d12f64
tjorven <tjorvenliebe@gmail.com>
2024-07-16 22:24:55 +0200

    fix wrong import

--- src/main/java/fuck/you/minecraft/MinecraftServerHandler.java
+++ src/main/java/fuck/you/minecraft/MinecraftServerHandler.java
@@ -4,7 +4,6 @@ import fuck.you.minecraft.motd.*;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
-import motd.*;
 
 public class MinecraftServerHandler extends SimpleChannelInboundHandler<ByteBuf> {
 
